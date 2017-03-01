6d11de4bbb697b2cc73ee48565177d9a12a7cf2d
nomaddc <g.freemanhl@gmail.com>
2017-03-01 17:45:50 +0300

    fixes, validation

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,8 @@
 class Article < ApplicationRecord
+  validates :title_rus, presence: true, length: { minimum: 5 }
+  validates :country, presence: true, length: { minimum: 5 }
+  validates :src_name, presence: true, length: { minimum: 5 }
+  validates :src_url, presence: true
+  validates :keywords, presence: true
+  validates :text, presence: true, length: { minimum: 5 }
 end
