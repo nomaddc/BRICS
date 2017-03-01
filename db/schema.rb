e9049a52c2cf4704f6d2b23d22bdabf393cf06c5
nomaddc <g.freemanhl@gmail.com>
2017-03-01 17:03:38 +0300

    Translated title add

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170224153103) do
+ActiveRecord::Schema.define(version: 20170301135519) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20170224153103) do
     t.text     "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "title_rus"
   end
 
 end
