5efd076ddd52fbe601aea10310c055832b0a7219
nomaddc <g.freemanhl@gmail.com>
2017-03-17 13:43:47 +0300

    req

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,7 @@
 class Article < ApplicationRecord
   validates :title_rus, presence: true, length: { minimum: 5 }
-  validates :country, presence: true, length: { minimum: 5 }
-  validates :src_name, presence: true, length: { minimum: 5 }
+  validates :country, presence: true, length: { minimum: 2 }
+  validates :category, presence: true, length: { minimum: 3 }
   validates :src_url, presence: true
   validates :keywords, presence: true
   validates :text, presence: true, length: { minimum: 5 }
