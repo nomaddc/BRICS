a3f6f90a56a3d59630adac1fa3eaed3bab50d97f
nomaddc <g.freemanhl@gmail.com>
2017-03-22 17:03:06 +0300

    link

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -44,7 +44,7 @@ class ArticlesController < ApplicationController
  
     redirect_to articles_path
   end
- 
+
   private
     def article_params
       params.require(:article).permit(:title_rus, :title, :date, :src_url, :country, :author, :category, :keywords, :text)
