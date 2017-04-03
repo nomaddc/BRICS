78dc4fa10a627b71bf158312caadea01a5464919
nomaddc <g.freemanhl@gmail.com>
2017-04-04 02:07:45 +0300

    filters

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,5 @@
 class Article < ApplicationRecord
+  include Filterable
   validates :title_rus, presence: true, length: { minimum: 5 }
   validates :country, presence: true, length: { minimum: 2 }
   validates :category, presence: true
@@ -6,4 +7,13 @@ class Article < ApplicationRecord
   validates :keywords, presence: true
   validates :text, presence: true, length: { minimum: 5 }
   validates :date, presence: true
+
+  scope :country, -> (country) { where country: country }
+  scope :category, -> (category) { where category: category }
+  scope :src_url, -> (src_url) { where src_url: src_url }
+  scope :date, -> (date) { where date: date }
+  scope :lang, -> (lang) { where lang: lang }
+  scope :keywords, -> (keywords) { where keywords: keywords }
+  # scope :title_rus, -> (title_rus) { where('title_rus like ?', '#{title_rus}%')}
+  # scope :title, -> (title) { where('title like ?', '#{title}%')}
 end
