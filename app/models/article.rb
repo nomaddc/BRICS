312c8c18b9a65cee108da2dba2517b292c3c5bb6
nomaddc <g.freemanhl@gmail.com>
2017-04-05 20:53:51 +0300

    filters

--- app/models/article.rb
+++ app/models/article.rb
@@ -10,10 +10,10 @@ class Article < ApplicationRecord
 
   scope :country, -> (country) { where country: country }
   scope :category, -> (category) { where category: category }
-  scope :src_url, -> (src_url) { where src_url: src_url }
+  scope :src_url, -> (src_url) { where('src_url like ?', "%#{src_url}%")}
   scope :date, -> (date) { where date: date }
   scope :lang, -> (lang) { where lang: lang }
-  scope :keywords, -> (keywords) { where keywords: keywords }
-  # scope :title_rus, -> (title_rus) { where('title_rus like ?', '#{title_rus}%')}
-  # scope :title, -> (title) { where('title like ?', '#{title}%')}
+  scope :keywords, -> (keywords) { where('keywords like ?', "%#{keywords}%")}
+  scope :title_rus, -> (title_rus) { where('title_rus like ?', "%#{title_rus}%")}
+  scope :title, -> (title) { where('title LIKE ?', "%#{title}%")}
 end
