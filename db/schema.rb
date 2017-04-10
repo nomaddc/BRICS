87dae0cf617f3bd0cf80daae0ab8661e004ed84e
nomaddc <g.freemanhl@gmail.com>
2017-04-10 04:28:44 +0300

    rev

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170331200203) do
+ActiveRecord::Schema.define(version: 20170410010617) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
