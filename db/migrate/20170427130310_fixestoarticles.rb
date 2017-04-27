d5f5f72578a97608f05f172b9b190bf01ca6f7e2
nomaddc <g.freemanhl@gmail.com>
2017-04-27 16:10:55 +0300

    fixes

---
+++ db/migrate/20170427130310_fixestoarticles.rb
@@ -0,0 +1,16 @@
+﻿class Fixestoarticles < ActiveRecord::Migration[5.0]
+
+  def change
+
+    ActiveRecord::Base.transaction do
+      Article.all.each do |article|
+        Article.where(:country => "Russia").update_all(:country => "Russian Federation")
+        Article.where(:country => "Китай").update_all(:country => "China")
+        Article.where(:category => "Внутреняя политика").update_all(:category => "Внутренняя политика")
+        Article.where(:category => "economics").update_all(:category => "investment and finance")
+        Article.where(:category => "domestic policy").update_all(:category => "political events")
+      end
+    end
+  end
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170410010617) do
+ActiveRecord::Schema.define(version: 20170427130310) do
 
   create_table "articles", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "title"
