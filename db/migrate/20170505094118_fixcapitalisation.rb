eb93df48f21e6e7ddf0a3bfb12b6351a82019524
nomaddc <g.freemanhl@gmail.com>
2017-05-05 12:59:15 +0300

    db fix

---
+++ db/migrate/20170505094118_fixcapitalisation.rb
@@ -0,0 +1,17 @@
+﻿class Fixcapitalisation < ActiveRecord::Migration[5.0]
+
+  def change
+
+    ActiveRecord::Base.transaction do
+      Article.all.each do |article|
+        Article.where(:category => "Международные отношения").update_all(:category => "международные отношения")
+        Article.where(:category => "Мир Труда").update_all(:category => "мир труда")
+        Article.where(:category => "Внутренняя политика").update_all(:category => "внутренняя политика")
+        Article.where(:category => "Экономика").update_all(:category => "экономика")
+        Article.where(:category => "World of Work").update_all(:category => "world of work")
+        Article.where(:category => "International relations").update_all(:category => "international relations")
+        Article.where(:category => "Documents").update_all(:category => "documents")
+      end
+    end
+  end
+end
