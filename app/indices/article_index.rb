1049340f69184c172be1ebf879872a44c9930598
nomaddc <g.freemanhl@gmail.com>
2017-05-11 17:47:13 +0300

    upd

--- app/indices/article_index.rb
+++ app/indices/article_index.rb
@@ -1,15 +1,37 @@
-ThinkingSphinx::Index.define :article, :with => :active_record do
-  # fields
-  indexes title
-  indexes country, :sortable => true
-  indexes keywords
-  indexes title_rus
-  indexes category, :sortable => true
-  indexes text
-  indexes lang
-  indexes date
-  indexes src_url
-  indexes author
+ThinkingSphinx::Index.define(
+  :article, :name => 'ind', :with => :active_record
+  ) do
+    # fields
+    indexes title
+    indexes country, :sortable => true
+    indexes keywords
+    indexes title_rus
+    indexes category, :sortable => true
+    indexes text
+    indexes lang
+    indexes date
+    indexes src_url
+    indexes author
 
-  has created_at, updated_at, is_active
+    has created_at, updated_at, is_active
+end
+
+ThinkingSphinx::Index.define(
+  :article, :name => 'rkwd_ind', :with => :active_record
+  ) do
+    indexes keywords
+    where "lang = 'rus'"
+end
+
+ThinkingSphinx::Index.define(
+  :article, :name => 'ekwd_ind', :with => :active_record
+  ) do
+    indexes keywords
+    where "lang = 'eng'"
+end
+
+ThinkingSphinx::Index.define(
+  :article, :name => 'kwd_ind', :with => :active_record
+  ) do
+    indexes keywords
 end
